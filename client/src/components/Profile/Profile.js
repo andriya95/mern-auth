1d29102409c1a6bb3d1ad3b80e6cea709c75bc30
andriya95 <andriyastokic@gmail.com>
2022-09-01 14:35:23 +0200

    adding update user and reset password feature

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -3,6 +3,7 @@ import Avatar from '../Avatar/Avatar';
 import {AiFillCamera} from 'react-icons/ai';
 import {MdVisibility} from 'react-icons/md';
 import {MdVisibilityOff} from 'react-icons/md';
+import { isLength, isMatch } from "../helper/validate";
 import { useState, useRef, useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import './profile.css'
@@ -11,12 +12,20 @@ import "react-toastify/dist/ReactToastify.css";
 import axios from 'axios';
 
 
+const initialState = {
+  name: '',
+  password: '',
+  cf_password: ''
+};
+
 
 const Profile = () => {
   const inputFile = useRef(null);
   const [visible, setVisible] = useState(false);
-  const { user, token } = useContext(AuthContext);
+  const { user, token, dispatch } = useContext(AuthContext);
   const [avatar, setAvatar] = useState(false);
+  const [data, setData] = useState(initialState);
+  const { name, password, cf_password } = data;
 
   const handleInput = () => {
     inputFile.current.click();
@@ -24,6 +33,81 @@ const Profile = () => {
 
   const handleClick = () => {
     setVisible(!visible);
+  };
+
+  const handleChange = (e) => {
+    setData({...data, [e.target.name]: e.target.value});
+  };
+
+  const updateInfo = async () => {
+    try {
+      const res = await axios.patch('/v1/api/auth/user_update', {
+        name: name ? name : user.name,
+        avatar: avatar ? avatar : user.avatar
+      }, {
+        headers: {Authorization: token}
+      });
+
+      const updatedUser = await axios.get('/v1/api/auth/user', {
+        headers: {Authorization: token}
+      });
+
+      dispatch({ type: "GET_USER", payload: updatedUser.data });
+
+      return toast(res.data.message, {
+        className: 'toast-success',
+        bodyClassName: 'toast-success'
+      });
+
+    } catch(err) {
+      toast(err.response.data.error, {
+        className: 'toast-failed',
+        bodyClassName: 'toast-failed'
+      });
+    }
+  };
+
+  const updatePassword = async () => {
+    //check password length
+    if (isLength(password)) {
+      return toast('Password must be at least 6 characters long.', {
+        className: 'toast-failed',
+        bodyClassName: 'toast-failed'
+      });
+    }
+    //check if passwords are matching
+    if (!isMatch(password, cf_password)) {
+      return toast('Passwords must match.', {
+        className: 'toast-failed',
+        bodyClassName: 'toast-failed'
+      });
+    }
+
+    try {
+      const res = await axios.post('/v1/api/auth/reset_pass', { password },
+      {
+        headers: {Authorization: token}
+      });
+      return toast(res.data.message, {
+        className: 'toast-success',
+        bodyClassName: 'toast-success'
+      });
+    } catch(err) {
+      toast(err.response.data.error, {
+        className: 'toast-failed',
+        bodyClassName: 'toast-failed'
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name || avatar) {
+      updateInfo();
+    }
+    if (password) {
+      updatePassword();
+    }
   };
 
   const changeAvatar = async (e) => {
@@ -57,7 +141,7 @@ const Profile = () => {
         bodyClassName: 'toast-failed'
       })
     }
-  }
+  };
 
   return (
     <>
@@ -78,24 +162,41 @@ const Profile = () => {
         />
       </div>
       {/* form */}
-      <form className="profile_input">
+      <form className="profile_input" onSubmit={handleSubmit}>
         <div className="profile_input-form">
-          <Input type="text" text="Name" defaultValue={user.name} />
-          <Input type="text" text="Email" defaultValue={user.email} disabled />
+          <Input 
+            name="name" 
+            type="text" 
+            text="Name" 
+            defaultValue={user.name}
+            handleChange={handleChange}  
+          />
+          <Input 
+            name="email"
+            type="text" 
+            text="Email" 
+            defaultValue={user.email} 
+            disabled
+            handleChange={handleChange}  
+          />
           <Input
+            name="password"
             type={visible ? "text" : "password"}
             icon={visible ? <MdVisibility /> : <MdVisibilityOff />}
             text="Password"
             handleClick={handleClick}
+            handleChange={handleChange}
           />
           <Input
+            name="cf_password"
             type={visible ? "text" : "password"}
             icon={visible ? <MdVisibility /> : <MdVisibilityOff />}
             text="Confirm Password"
             handleClick={handleClick}
+            handleChange={handleChange}
           />
           <div className="login_btn">
-            <button>update</button>
+            <button type="submit">update</button>
           </div>
         </div>
       </form>
